61eaa9d011db26cddc494514ce56cefe3ceab70c
MayankKumar1991 <mayankkumark1991@gmail.com>
2021-02-24 17:16:54 +0530

    ready for deploy

---
+++ src/components/Accordion.js
@@ -0,0 +1,29 @@
+import React,{ useState} from 'react';
+
+const Accordion = ({items}) => {
+    const [activeState,setActiveState] = useState(null);
+
+    const onTitleClick = (index) =>{
+        setActiveState(index);
+    }
+
+    const renderedItems = items.map((item , index) => {
+    const active = index === activeState ? 'active' : '';
+    
+    return (
+    <React.Fragment key={item.title}>
+    <div className={`title ${active}`} onClick={()=>onTitleClick(index)} >
+        <i className="dropdown icon"></i>
+        {item.title}
+    </div>
+    <div className={`content ${active}`}>
+        <p>{item.content}</p>
+    </div>
+    </React.Fragment>
+    );
+});
+   
+    return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
